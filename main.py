888f97d6223d3f383e375fa313ddefa5a0773a74
Heather Diamond <baishenkokv@gmail.com>
2025-04-13 21:32:24 +0300

    add dz

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-my_list = [1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8]
-print(my_list)
-print(len(my_list))
-my_set = set(my_list)
-print(my_set)
-print(len(my_set))
+def add_list():
+    user_string = input("Введите элементы списка через пробелы: ")
+    user_list = [int(num) for num in user_string.split()]
+    return user_list
+
+
+def show_info_about_user_list(user_list):
+    user_set = set(user_list)
+    print(f"""
+Введенный список элементов: {user_list}
+Количество элементов в введенном списке: {len(user_list)}
+
+Список уникальных элементов: {user_set}
+Количество элементов в введенном списке: {len(user_set)}""")
+
+
+user_list = add_list()
+show_info_about_user_list(user_list)
